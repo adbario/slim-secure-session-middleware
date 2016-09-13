c06596b3d7cb584eaf9a76803c67ec9b2b13e14f
AdBar <adbario@users.noreply.github.com>
2016-09-13 16:06:09 +0100

    Update Session.php

--- src/Session.php
+++ src/Session.php
@@ -6,21 +6,38 @@ namespace AdBar;
  * Session
  *
  * This class is a helper for setting and getting session values.
+ * Sessions also use namespaces.
  */
 class Session
 {
+    /** @var string Session namespace */
+    protected $namespace = 'slim_app';
+
+    /**
+     * Constructor
+     *
+     * @param string|null $namespace Session namespace
+     */
+    public function __construct($namespace = null)
+    {
+        if (is_string($namespace)) {
+            $this->setNamespace($namespace);
+        }
+        $this->validateNamespace($this->namespace);
+    }
+
     /**
      * Set session value or multiple values as an array
      *
      * @param string|array $key   Session key or an array of keys and values
      * @param mixed|null   $value Session value or null if $key is an array
      */
-    public static function set($key, $value = null)
+    public function set($key, $value = null)
     {
         if (is_array($key)) {
-            $_SESSION = array_merge($_SESSION, $key);
+            $_SESSION[$this->namespace] = array_merge($_SESSION[$this->namespace], $key);
         } else {
-            $_SESSION[$key] = $value;
+            $_SESSION[$this->namespace][$key] = $value;
         }
     }
 
@@ -32,11 +49,11 @@ class Session
      * @param  mixed  $default Default value
      * @return mixed
      */
-    public static function get($key = null, $default = null)
+    public function get($key = null, $default = null)
     {
         return $key === null
-            ? $_SESSION
-            : self::has($key) ? $_SESSION[$key] : $default;
+            ? $_SESSION[$this->namespace]
+            : $this->has($key) ? $_SESSION[$this->namespace][$key] : $default;
     }
 
     /**
@@ -45,9 +62,9 @@ class Session
      * @param  string $key Session key
      * @return bool
      */
-    public static function has($key)
+    public function has($key)
     {
-        return array_key_exists($key, $_SESSION);
+        return array_key_exists($key, $_SESSION[$this->namespace]);
     }
 
     /**
@@ -55,19 +72,19 @@ class Session
      *
      * @param string $key Session key
      */
-    public static function delete($key)
+    public function delete($key)
     {
-        if (self::has($key)) {
-            unset($_SESSION[$key]);
+        if ($this->has($key)) {
+            unset($_SESSION[$this->namespace][$key]);
         }
     }
 
     /**
      * Delete all session values
      */
-    public static function clear()
+    public function clear()
     {
-        $_SESSION = [];
+        $_SESSION[$this->namespace] = [];
     }
 
     /**
@@ -78,6 +95,108 @@ class Session
     public static function isActive()
     {
         return session_status() === PHP_SESSION_ACTIVE;
+    }
+
+    /**
+     * Set session namespace
+     *
+     * @param string $namespace Session namespace
+     */
+    public function setNamespace($namespace)
+    {
+        $this->namespace = $namespace;
+        $this->validateNamespace($namespace);
+    }
+
+    /**
+     * Get session namespace
+     *
+     * @return string Session namespace
+     */
+    public function getNamespace()
+    {
+        return $this->namespace;
+    }
+
+    /**
+     * Create array for namespace if it doesn't exist
+     */
+    protected function validateNamespace($namespace)
+    {
+        if (!isset($_SESSION[$namespace])) {
+            $_SESSION[$namespace] = [];
+        }
+    }
+
+    /**
+     * Set session value(s) to specific namespace
+     *
+     * @param string       $namespace Session namespace
+     * @param string|array $key       Session key or an array of keys and values
+     * @param mixed|null   $value     Session value or null if $key is an array
+     */
+    public function setTo($namespace, $key, $value = null)
+    {
+        if (is_array($key)) {
+            $this->validateNamespace($namespace);
+            $_SESSION[$namespace] = array_merge($_SESSION[$namespace], $key);
+        } else {
+            $_SESSION[$namespace][$key] = $value;
+        }
+    }
+
+    /**
+     * Get session value from specific namespace
+     *
+     * @param  string $namespace Session namespace
+     * @param  string $key       Session key
+     * @param  mixed  $default   Default value
+     * @return mixed
+     */
+    public function getFrom($namespace, $key = null, $default = null)
+    {
+        $this->validateNamespace($namespace);
+
+        return $key === null
+            ? $_SESSION[$namespace]
+            : $this->hasIn($namespace, $key) ? $_SESSION[$namespace][$key] : $default;
+    }
+
+    /**
+     * Check if session value is set in specific namespace
+     *
+     * @param  string $namespace Session namespace
+     * @param  string $key       Session key
+     * @return bool
+     */
+    public function hasIn($namespace, $key)
+    {
+        $this->validateNamespace($namespace);
+
+        return array_key_exists($key, $_SESSION[$namespace]);
+    }
+
+    /**
+     * Delete session value from specific namespace
+     *
+     * @param string $namespace Session namespace
+     * @param string $key       Session key
+     */
+    public function deleteFrom($namespace, $key)
+    {
+        if ($this->hasIn($namespace, $key)) {
+            unset($_SESSION[$namespace][$key]);
+        }
+    }
+
+    /**
+     * Delete all session values from specific namespace
+     *
+     * @param  string $namespace Session namespace
+     */
+    public function clearFrom($namespace)
+    {
+        $_SESSION[$namespace] = [];
     }
 
     /**
